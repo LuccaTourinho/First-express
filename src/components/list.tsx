b0b5d5c1fb4dca64612693743276c975688fe7e0
Lucca Tourinho <luccatourinho@gmail.com>
2024-07-27 00:30:39 -0300

    Finish Pagination

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Pagination from './pagination';
 import { useTaskContext } from '../context/task';
 import ToDo from './todo';
 
+const ITEMS_PER_PAGE = 10;
 
 interface IListProps {
 }
 
 const List: React.FunctionComponent<IListProps> = (props) => {
   const { tasks } = useTaskContext();
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  const totalPages = Math.ceil(tasks.length / ITEMS_PER_PAGE);
+
+  const currentTasks = tasks.slice((currentPage - 1) * ITEMS_PER_PAGE, currentPage * ITEMS_PER_PAGE);
 
   return (
     <div className='flex flex-col w-[500px] h-full p-2'>
         <div className='flex flex-col gap-1 p-1 w-full h-full'>
           {
-            tasks.map(task => (
+            currentTasks.map(task => (
               <ToDo key={task.id} task={task} />
             ))
           }
         </div>
-        <Pagination/>
+        <Pagination currentPage={currentPage} onPageChange={(page) => setCurrentPage(page)} totalPages={totalPages}/>
     </div>
   );
 };
--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -1,15 +1,39 @@
 import React from 'react';
 
 interface IPaginationProps {
+  currentPage: number;
+  onPageChange: (page: number) => void;
+  totalPages: number;
 }
 
 const Pagination: React.FunctionComponent<IPaginationProps> = (props) => {
+
+  const startPage = Math.max(props.currentPage - 2, 1);
+  const endPage = Math.min(props.currentPage + 2, props.totalPages);
+  const pages = [];
+
+  for (let i = startPage; i <= endPage; i++) {
+    pages.push(i);
+  }
+
   return (
     <div className='flex w-full justify-around p-4'>
-        <i className="fa-solid fa-angles-left text-white"></i>
-        <i className="fa-solid fa-angle-left text-white"></i>
-        <i className="fa-solid fa-angle-right text-white"></i>
-        <i className="fa-solid fa-angles-right text-white"></i>
+        <i 
+          onClick={() => props.currentPage > 1 && props.onPageChange(1)} 
+          className={`fa-solid fa-angles-left text-white ${props.currentPage === 1 ? 'text-gray-500' : 'cursor-pointer transform transition-transform duration-300 hover:scale-125'}`}></i>
+        {
+          pages.map(page => (
+            <span
+              key={page}
+              className={`text-white ${page === props.currentPage ? 'font-bold text-gray-500 border-b-2 border-gray-500' : 'cursor-pointer transform transition-transform duration-300 hover:scale-125'}`}
+              onClick={() => props.onPageChange(page)}
+            >
+              {page}
+            </span>
+        ))}
+        <i 
+          onClick={() => props.currentPage < props.totalPages && props.onPageChange(props.totalPages)} 
+          className={`fa-solid fa-angles-right text-white ${props.currentPage === props.totalPages ? 'text-gray-500' : 'cursor-pointer transform transition-transform duration-300 hover:scale-125'}`}></i>
     </div>
   );
 };
